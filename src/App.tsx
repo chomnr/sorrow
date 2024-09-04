bf16068db03594fe4fae396efd49169b42a46675
chomnr <zeljkovranjes@outlook.com>
2024-09-04 13:39:30 -0400

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Instances, Lightformer, MeshReflectorMaterial, OrbitControls } from "@react-three/drei";
+import { Effects } from "./Effects";
+import { Robot } from "./Robot";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      gl={{ antialias: false }}
+      dpr={[1, 1.5]}
+      camera={{ position: [0, 0, 15], fov: 25 }}
+    >
+      <color attach="background" args={["#15151a"]} />
+      <mesh scale={4} position={[3, -1.161, -1.5]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
+        <ringGeometry args={[0.9, 1, 4, 1]} />
+        <meshStandardMaterial color="red" roughness={0.75} />
+      </mesh>
+      <mesh scale={4} position={[-3, -1.161, -1]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
+        <ringGeometry args={[0.9, 1, 3, 1]} />
+        <meshStandardMaterial color="red" roughness={0.75} />
+      </mesh>
+      <group position={[-0, -1, 0]}>
+        <Robot />
+        {/* Plane reflections + distance blur */}
+        <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
+          <planeGeometry args={[50, 50]} />
+          <MeshReflectorMaterial
+            blur={[300, 30]}
+            resolution={2048}
+            mixBlur={1}
+            mixStrength={180}
+            roughness={1}
+            depthScale={1.2}
+            minDepthThreshold={0.4}
+            maxDepthThreshold={1.4}
+            color="#202020"
+            metalness={0.8} mirror={0}          />
+        </mesh>
+        {/* Bunny and a light give it more realism */}
+      </group>
+      <Environment resolution={512}>
+        {/* Ceiling */}
+        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -9]} scale={[10, 1, 1]} />
+        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -6]} scale={[10, 1, 1]} />
+        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -3]} scale={[10, 1, 1]} />
+        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 0]} scale={[10, 1, 1]} />
+        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 3]} scale={[10, 1, 1]} />
+        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 6]} scale={[10, 1, 1]} />
+        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 9]} scale={[10, 1, 1]} />
+        {/* Sides */}
+        <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} />
+        <Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} />
+        {/* Key */}
+        <Lightformer form="ring" color="red" intensity={10} scale={2} position={[10, 5, 10]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
+      </Environment>
+      <Effects />
+      <OrbitControls enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI / 2.2} />
+    </Canvas>
   );
 }
 
