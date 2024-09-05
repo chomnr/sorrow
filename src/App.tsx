feaac04071a5f49734006d146a2bc0c9fbc7b4bc
chomnr <zeljkovranjes@outlook.com>
2024-09-04 21:36:52 -0400

    added camera lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,120 @@
 import React from "react";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import { Environment, Instances, Lightformer, MeshReflectorMaterial, OrbitControls } from "@react-three/drei";
+import {
+  Environment,
+  Lightformer,
+  MeshReflectorMaterial,
+  OrbitControls,
+} from "@react-three/drei";
 import { Effects } from "./Effects";
 import { Robot } from "./Robot";
 
 function App() {
   return (
-    <Canvas
-      gl={{ antialias: false }}
-      dpr={[1, 1.5]}
-      camera={{ position: [-60, 30, 30], fov: 7, rotation: [0, 0, 0] }}
-    >
-      <color attach="background" args={["#101010"]} />
-      <mesh scale={4} position={[3, -1.161, -1.5]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
-        <ringGeometry args={[0.9, 1, 4, 1]} />
-        <meshStandardMaterial color="red" roughness={0.75} />
-      </mesh>
-      <mesh scale={4} position={[-3, -1.161, -1]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
-        <ringGeometry args={[0.9, 1, 3, 1]} />
-        <meshStandardMaterial color="red" roughness={0.75} />
-      </mesh>
-      <group position={[-0, -1, 0]}>
-        <Robot />
-        {/* Plane reflections + distance blur */}
-        <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
-          <planeGeometry args={[50, 50]} />
-          <MeshReflectorMaterial
-            blur={[300, 30]}
-            resolution={2048}
-            mixBlur={1}
-            mixStrength={180}
-            roughness={1}
-            depthScale={1.2}
-            minDepthThreshold={0.4}
-            maxDepthThreshold={1.4}
-            color="#202020"
-            metalness={0.8} mirror={0}          />
-        </mesh>
-        {/* Bunny and a light give it more realism */}
-      </group>
-      <Environment resolution={512}>
-        {/* Ceiling */}
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -9]} scale={[10, 1, 1]} />
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -6]} scale={[10, 1, 1]} />
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -3]} scale={[10, 1, 1]} />
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 0]} scale={[10, 1, 1]} />
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 3]} scale={[10, 1, 1]} />
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 6]} scale={[10, 1, 1]} />
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 9]} scale={[10, 1, 1]} />
-        {/* Sides */}
-        <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} />
-        <Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} />
-        {/* Key */}
-        <Lightformer form="ring" color="red" intensity={10} scale={2} position={[10, 5, 10]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
-      </Environment>
-      <Effects />
-      {/* <OrbitControls enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI / 2.2} /> */}
-      <OrbitControls />
-    </Canvas>
+    <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
+      {/* WebGL Canvas */}
+      <Canvas
+        gl={{ antialias: false }}
+        dpr={[1, 1.5]}
+        camera={{ position: [-60, 30, 30], fov: 5, rotation: [0, 0, 0] }}
+      >
+        <color attach="background" args={["#101010"]} />
+        <group position={[-0, -1, 0]}>
+          <Robot />
+          {/* Plane reflections + distance blur */}
+          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
+            <planeGeometry args={[50, 50]} />
+            <MeshReflectorMaterial
+              blur={[300, 30]}
+              resolution={2048}
+              mixBlur={1}
+              mixStrength={180}
+              roughness={1}
+              depthScale={1.2}
+              minDepthThreshold={0.4}
+              maxDepthThreshold={1.4}
+              color="#202020"
+              metalness={0.8}
+              mirror={0}
+            />
+          </mesh>
+        </group>
+        <Environment resolution={512}>
+          {/* Ceiling Lights */}
+          <Lightformer
+            intensity={2}
+            rotation-x={Math.PI / 2}
+            position={[0, 4, -9]}
+            scale={[10, 1, 1]}
+          />
+          <Lightformer
+            intensity={2}
+            rotation-x={Math.PI / 2}
+            position={[0, 4, 0]}
+            scale={[10, 1, 1]}
+          />
+          {/* Sides */}
+          <Lightformer
+            intensity={2}
+            rotation-y={Math.PI / 2}
+            position={[-50, 2, 0]}
+            scale={[100, 2, 1]}
+          />
+          <Lightformer
+            intensity={2}
+            rotation-y={-Math.PI / 2}
+            position={[50, 2, 0]}
+            scale={[100, 2, 1]}
+          />
+          {/* Key */}
+          <Lightformer
+            form="ring"
+            color="red"
+            intensity={10}
+            scale={2}
+            position={[10, 5, 10]}
+            onUpdate={(self) => self.lookAt(0, 0, 0)}
+          />
+        </Environment>
+        <Effects />
+        <OrbitControls
+          enableDamping={false}
+          enableZoom={false}
+          enableRotate={false}
+        />
+      </Canvas>
+
+      {/* HTML Overlay */}
+      <div className="cctv">
+        {/* LEFT */}
+        <div className="cctv-line-group">
+          <div className="cam-top-line" />
+          <div className="cam-line" />
+        </div>
+
+        {/* BOTTOM LEFT */}
+        <div
+          className="cctv-line-group"
+          style={{ bottom: "13px", transform: "rotate(270deg)" }}
+        >
+          <div className="cam-top-line" />
+          <div className="cam-line" />
+        </div>
+
+        {/* TOP RIGHT */}
+        <div className="cctv-line-group" style={{right: '13px', transform: 'rotate(90deg)'}}>
+          <div className="cam-top-line" />
+          <div className="cam-line-v2" />
+        </div>
+
+        {/* BOTTOM RIGHT */}
+        <div className="cctv-line-group" style={{bottom: '13px', right: '13px', transform: 'rotate(270deg)'}}>
+          <div className="cam-line-v2" />
+          <div className="cam-top-line" />
+        </div>
+      </div>
+    </div>
   );
 }
 
