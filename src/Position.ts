33ff716851be2425fc3d2c2082e6bb5adc6fdcc4
chomnr <zeljkovranjes@outlook.com>
2024-09-10 12:14:56 -0400

    adjusted initial position

--- src/Position.ts
+++ src/Position.ts
@@ -3,7 +3,8 @@ import { Color, Euler, Vector3 } from "three";
 
 export const BACKGROUND_COLOR: Color = new Color("#202020")
 
-export var IN_POSITION = new Vector3(-60, 30, 30)
+// export var IN_POSITION = new Vector3(-60, 30, 30)
+export var IN_POSITION = new Vector3(0, 13, 13)
 export const IN_DPR = [1, 1.5] as Dpr
-export const IN_FOV = 5;
+export const IN_FOV = 17;
 export const IN_ROTATION = new Euler(0, 0, 0);
