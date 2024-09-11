dc62b75e40d39b12ee57700a054c02c30ed603d1
chomnr <zeljkovranjes@outlook.com>
2024-09-10 22:46:53 -0400

    removed commented code

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import { IN_DPR, IN_POSITION, IN_FOV, IN_ROTATION } from "./Position";
 import { Scene } from "./component/Scene";
-import { Loader } from "@react-three/drei";
 import { Suspense } from "react";
 import {
   EffectComposer,
@@ -15,7 +14,7 @@ import {
 import { LoadingScreen } from "./component/LoadingScreen";
 import { LoadingProvider } from "./context/LoadingContext";
 import { BackgroundSound } from "./component/BackgroundSound";
-import { SoundContext, ToggleableSound } from "./context/SoundContext";
+import { ToggleableSound } from "./context/SoundContext";
 
 function App() {
   return (
@@ -55,67 +54,5 @@ export function Effect() {
     </EffectComposer>
   );
 }
-
-/*
-export enum CameraLocation {
-  INITIAL,
-  MONITOR
-}
-
-export enum AppState {
-  LOADING,
-  LOADED
-}
-
-
-export const BACKGROUND = ["background", "#0A0A0A"];
-export const DPR = [1, 1.5] as Dpr;
-
-export const INITIAL_POSITION = [-60, 30, 30];
-export const INITIAL_FOV = 5;
-export const INITIAL_ROTATION = [0, 0, 0];
-
-export const MONITOR_POSITION = [0.225, 1, 18];
-export const MONITOR_FOV = 5;
-export const MONITOR_ROTATION = [0, 0, 0];
-
-export let CURRENT_CAMERA_LOCATION: CameraLocation = CameraLocation.INITIAL
-
-export let CURRENT_APP_STATE: AppState = AppState.LOADING;
-
-function App() {
-  return (
-    <>
-      <Canvas
-        gl={{ antialias: false }}
-        dpr={DPR}
-        camera={{
-          position: new Vector3(INITIAL_POSITION[0], INITIAL_POSITION[1], INITIAL_POSITION[2]),
-          fov: INITIAL_FOV,
-          rotation: new Euler(INITIAL_ROTATION[0], INITIAL_ROTATION[1], INITIAL_ROTATION[2]),
-        }}
-      >
-      <Scene/>
-      </Canvas>
-      <ToggleableSound>
-        <Overlay/>
-      </ToggleableSound>
-    </>
-  );
-}
-
-
-export function updateCameraLocation(cameraLocation: CameraLocation) {
-  CURRENT_CAMERA_LOCATION = cameraLocation
-}
-
-export function updateAppState(appState: AppState) {
-  CURRENT_APP_STATE = appState
-}
-*/
-
-/**
- * Background Sound
- */
 
 export default App;
